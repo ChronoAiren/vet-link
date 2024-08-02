e2e965f1b9f6963cfd4968341d51193de32f6dba
Emmalyn Nabiamos <emmy2699@gmail.com>
2024-08-02 10:26:32 +0800

    Added initial theming

--- frontend/lib/styles/readme.md
+++
@@ -1,1 +0,0 @@
-Directory for Styles, such as Colors, Typography, etc.
---
+++ frontend/lib/styles/text_styles.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+// Header
+const TextStyle headerBoldPoppins = TextStyle(
+  fontSize: 42,
+  fontWeight: FontWeight.w700,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle headerSemiboldPoppins = TextStyle(
+  fontSize: 42,
+  fontWeight: FontWeight.w600,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle headerRegularPoppins = TextStyle(
+  fontSize: 42,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle headerThinPoppins = TextStyle(
+  fontSize: 42,
+  fontWeight: FontWeight.w300,
+  fontFamily: 'Poppins',
+);
+
+// Subtitle
+const TextStyle subtitleBoldPoppins = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w700,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle subtitleSemiboldPoppins = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w600,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle subtitleRegularPoppins = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle subtitleThinPoppins = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w300,
+  fontFamily: 'Poppins',
+);
+
+// Body
+const TextStyle bodyBoldPoppins = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w700,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle bodySemiboldPoppins = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle bodyRegularPoppins = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle bodyThinPoppins = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w300,
+  fontFamily: 'Poppins',
+);
+
+// Small Text
+const TextStyle smallBoldPoppins = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w700,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle smallSemiboldPoppins = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle smallRegularPoppins = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle smallThinPoppins = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w300,
+  fontFamily: 'Poppins',
+);
+
+// Caption
+const TextStyle captionBoldPoppins = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w700,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle captionSemiboldPoppins = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w600,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle captionRegularPoppins = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle captionThinPoppins = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w300,
+  fontFamily: 'Poppins',
+);
+
+// Button Text
+const TextStyle buttonTextBoldPoppins = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w700,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle buttonTextSemiboldPoppins = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle buttonTextRegularPoppins = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Poppins',
+);
+
+const TextStyle buttonTextThinPoppins = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w300,
+  fontFamily: 'Poppins',
+);
