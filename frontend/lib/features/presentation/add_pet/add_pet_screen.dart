d5aa626efdbb6ccc39c9a15b39f019d12aa30ce3
RuthieXIT <patebriones@gmail.com>
2024-08-13 17:01:38 +0800

    Add the view pets and add pet

---
+++ frontend/lib/features/presentation/add_pet/add_pet_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/common/widgets/main_wrapper.dart';
+import 'package:get/get.dart';
+
+class AddPetScreen extends StatelessWidget {
+  const AddPetScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MainWrapper(
+      content: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              child: Text('add pet screen'),
+            )
+          ],
+        ),
+      ),
+      fab: FloatingActionButton(
+          shape: CircleBorder(),
+          backgroundColor: Colors.limeAccent,
+          onPressed: () {
+            Get.back();
+          }),
+    );
+  }
+}
